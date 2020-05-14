2398cd3531ba051de17cbd474703d1fb672513e0
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-14 18:29:55 +0530

    Update twoMultAdd.v

--- twoMultAdd.v
+++ twoMultAdd.v
@@ -1,18 +1,16 @@
-module twoMultAdd(W,X,U,h,b, out);	
+module twoMultAdd(X, w, b, out);	
 	// incoming data signed and fixed width
 	parameter DATA_WIDTH = 16;
 	parameter FRACT_WIDTH = 8;
 	
-	input signed [DATA_WIDTH-1:0] W,X,U,h,b;
+	input signed [DATA_WIDTH-1:0] X,w,b;
 	output wire [DATA_WIDTH-1:0] out;
-	
-	
+
 	// internal regs/wires
-	wire [DATA_WIDTH+FRACT_WIDTH-1:0] p1,p2;
+	wire [DATA_WIDTH+FRACT_WIDTH-1:0] p;
 	
-	// behavior: out = W*X + U*h + b
-	assign p1 = (W*X)>>FRACT_WIDTH;
-	assign p2 = (U*h)>>FRACT_WIDTH;
-	assign out = p1 + p2 + b;
+	// behavior: out = W*x + b
+	assign p = (W*X)>>FRACT_WIDTH;
+	assign out = p + b;
 
 endmodule
