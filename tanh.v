c37c0e2ad313d84570b2793eb690e3939f0a5d83
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-14 18:30:32 +0530

    Update tanh.v

--- tanh.v
+++ tanh.v
@@ -8,14 +8,7 @@ module tanh(X,Y);
 // define ports
 	input signed [DATA_WIDTH-1:0] X;
 	output wire [DATA_WIDTH-1:0] Y;
-	
-// internal use
-	//	wire [2*DATA_WIDTH-1:0] p1,p2;
-
-// module behavior:
-	//	assign p1 = 16'h0040*X; // 0.25*X
-	//	assign p2 = 16'h0098*X; // 0.5938*X = (1/2+1/16+1/32)*X
-	
+		
 	assign Y = (X[DATA_WIDTH-1]) ? (
 		// negative
 		(X < -16'h0100)? -16'h0100 : X )
