1db0ff0b83f7f37963f7a77535c4a62f10f289c8
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-14 18:58:17 +0530

    Update sigmoid.v

--- sigmoid.v
+++ sigmoid.v
@@ -2,10 +2,10 @@ module sigmoid(X,Y);
 	parameter DATA_WIDTH = 16;
 	parameter FRACT_WIDTH = 8;
 	
-	input signed [DATA_WIDTH-1:0] X;
+	input [DATA_WIDTH-1:0] X;
 	output wire [DATA_WIDTH-1:0] Y;
 	
-	wire signed [DATA_WIDTH-1:0] s1;
+	wire [DATA_WIDTH-1:0] s1;
 	assign s1 = X+16'h0200; 
 	
 	assign Y = (X[DATA_WIDTH-1]) ? (
