268dda43706971310540c3752dcf6973c4751c22
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-17 23:59:56 +0530

    Update sigmoid.v

--- sigmoid.v
+++ sigmoid.v
@@ -3,7 +3,7 @@ module sigmoid(X,Y);
 	parameter FRACT_WIDTH = 8;
 	
 	input signed [DATA_WIDTH-1:0] X;
-	output wire [DATA_WIDTH-1:0] Y;
+	output wire signed [DATA_WIDTH-1:0] Y;
 	
 	wire signed [DATA_WIDTH-1:0] s1;
 	assign s1 = X+16'h0200; 
