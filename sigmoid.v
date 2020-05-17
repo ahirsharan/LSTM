268ec911941a7595492821a82fe953bbf99d8917
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-17 23:26:08 +0530

    Update sigmoid.v

--- sigmoid.v
+++ sigmoid.v
@@ -2,13 +2,13 @@ module sigmoid(X,Y);
 	parameter DATA_WIDTH = 16;
 	parameter FRACT_WIDTH = 8;
 	
-	input signed [DATA_WIDTH-1:0] X;
+	input [DATA_WIDTH-1:0] X;
 	output wire [DATA_WIDTH-1:0] Y;
 	
 	wire [DATA_WIDTH-1:0] s1;
 	assign s1 = X+16'h0200; 
 	
-	assign Y = (X[DATA_WIDTH-1]) ? (
+	assign Y = (X<16'h0000) ? (
 		// negative
 		(X < -16'h0200)? 16'h0000 : (s1>>>2) ) 
 		// positive
