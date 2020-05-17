dbf78f6ae7a7c4661b047a4e386a3f2478f2f7dc
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-17 23:51:00 +0530

    Update lstm_cell.v

--- lstm_cell.v
+++ lstm_cell.v
@@ -10,16 +10,16 @@ module lstm_cell(c_in, h_in, X, c_out, h_out);
 	
 	//c_in : previous cell state 
 	//h_in: tanh(o_in)
-	input [DATA_WIDTH-1:0] c_in , h_in ;
+	input signed [DATA_WIDTH-1:0] c_in , h_in ;
 	
         //X: current input
-	input [DATA_WIDTH-1:0] X;
+	input signed [DATA_WIDTH-1:0] X;
 	
 	//Weight arrays : {Wf, Wi ,Wc, Wo} where each element will be of size 2 x 1
-	wire [DATA_WIDTH-1:0] Wf0, Wf1, Wi0, Wi1, Wc0, Wc1, Wo0, Wo1;
+	wire signed [DATA_WIDTH-1:0] Wf0, Wf1, Wi0, Wi1, Wc0, Wc1, Wo0, Wo1;
 	
 	//Bias arrays : {bf, bi ,bc, bo} where each element will be of size 1 x 1
-	wire [DATA_WIDTH-1:0] bf, bi, bc ,bo;
+	wire signed [DATA_WIDTH-1:0] bf, bi, bc ,bo;
 	
 	//Assign weights and biases
 	//Random values assigned
@@ -43,10 +43,10 @@ module lstm_cell(c_in, h_in, X, c_out, h_out);
 
 	//c_out : current cell state
 	//h_out : tanh(o_out)
-	output wire [DATA_WIDTH-1:0] c_out, h_out;
+	output wire signed [DATA_WIDTH-1:0] c_out, h_out;
 	
-	wire [DATA_WIDTH-1:0] f, i, iw, ct, cw, ot, out, c_act;
-	wire [DATA_WIDTH + FRACT_WIDTH - 1:0] z1, z2;
+	wire signed [DATA_WIDTH-1:0] f, i, iw, ct, cw, ot, out, c_act;
+	wire signed [DATA_WIDTH + FRACT_WIDTH - 1:0] z1, z2;
 	
 	//f= Wf*{X h_in} + bf
 	ConcatMultAdd A1(X, h_in, Wf0, Wf1, bf, f);
@@ -59,8 +59,8 @@ module lstm_cell(c_in, h_in, X, c_out, h_out);
 	ConcatMultAdd A3(X, h_in, Wc0, Wc1, bc, cw);
 	tanh T1(cw, ct);
 	
-	assign z1 = (f*c_in)>>FRACT_WIDTH;
-	assign z2 = (i*ct)>>FRACT_WIDTH;
+	assign z1 = (f*c_in)>>>FRACT_WIDTH;
+	assign z2 = (i*ct)>>>FRACT_WIDTH;
 	assign c_out = z1[DATA_WIDTH-1:0] + z2[DATA_WIDTH-1:0];
 	
 	//out=  sigmoid(Wo*{X h_in} + bo)
@@ -69,6 +69,6 @@ module lstm_cell(c_in, h_in, X, c_out, h_out);
 	
 	//h_out =(out*tanh(c_out))
 	tanh T2(c_out,c_act);
-	assign h_out = (out*c_act)>>FRACT_WIDTH;
+	assign h_out = (out*c_act)>>>FRACT_WIDTH;
 	
 endmodule
