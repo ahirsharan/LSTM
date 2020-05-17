f0854dd3f0367afb4eaa8c83a372706dbe6aa483
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-17 23:27:16 +0530

    Update tanh.v

--- tanh.v
+++ tanh.v
@@ -6,10 +6,10 @@ module tanh(X,Y);
 	parameter FRACT_WIDTH = 8;
 	
 // define ports
-	input signed [DATA_WIDTH-1:0] X;
+	input [DATA_WIDTH-1:0] X;
 	output wire [DATA_WIDTH-1:0] Y;
 		
-	assign Y = (X[DATA_WIDTH-1]) ? (
+	assign Y = (X<16'h0000) ? (
 		// negative
 		(X < -16'h0100)? -16'h0100 : X )
 		// positive
