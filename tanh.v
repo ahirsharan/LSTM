c26c11f20759f6b77515cee805a95fb9dd8768cc
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-17 23:39:05 +0530

    Update tanh.v

--- tanh.v
+++ tanh.v
@@ -9,7 +9,7 @@ module tanh(X,Y);
 	input [DATA_WIDTH-1:0] X;
 	output wire [DATA_WIDTH-1:0] Y;
 		
-	assign Y = (X<16'h0000) ? (
+	assign Y = (X[DATA_WIDTH-1]) ? (
 		// negative
 		(X < -16'h0100)? -16'h0100 : X )
 		// positive
