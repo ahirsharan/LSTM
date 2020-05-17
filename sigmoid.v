135d5c811ad215d7c2700687eed3274a1fd4e65a
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-17 23:59:21 +0530

    Update sigmoid.v

--- sigmoid.v
+++ sigmoid.v
@@ -5,7 +5,7 @@ module sigmoid(X,Y);
 	input signed [DATA_WIDTH-1:0] X;
 	output wire [DATA_WIDTH-1:0] Y;
 	
-	wire [DATA_WIDTH-1:0] s1;
+	wire signed [DATA_WIDTH-1:0] s1;
 	assign s1 = X+16'h0200; 
 	
 	assign Y = (X[DATA_WIDTH-1]) ? (
